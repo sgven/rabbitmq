1c7e79791e8ae33181722369ff4cb6ded45cc2a3
谢军 <sgvenday@outlook.com>
2019-09-11 12:59:12 +0800

    hello world

--- rabbitmqlab/lab01/src/main/java/learn/middleware/rabbitmq/demo/Send.java
+++ rabbitmqlab/lab01/src/main/java/learn/middleware/rabbitmq/demo/Send.java
@@ -9,14 +9,20 @@ public class Send {
     private final static String QUEUE_NAME = "hello";
 
     public static void main(String[] argv) throws Exception {
+        String message = "Hello World！";
+        sendMessage(QUEUE_NAME, message);
+    }
+
+    public static void sendMessage(String queueName, String message) throws Exception {
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
         try (Connection connection = factory.newConnection();
              Channel channel = connection.createChannel()) {
-            channel.queueDeclare(QUEUE_NAME,false,false,false,null);
-            String message = "Hello World！";
-            channel.basicPublish("",QUEUE_NAME,null,message.getBytes());
+            channel.queueDeclare(queueName,false,false,false,null);
+
+            channel.basicPublish("",queueName,null,message.getBytes());
             System.out.println("[x] Sent'" + message + "'");
         }
     }
+
 }
